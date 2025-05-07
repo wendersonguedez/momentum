76a47fe8beb2773100bdb030b8765a7e6963c3a6
wend <wendersonguedes6@gmail.com>
2025-05-06 23:26:19 -0300

    feat: implementada a listagem do historico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -19,38 +19,28 @@ export function History() {
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há cerca de 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há cerca de 2 meses</td>
-							<td>
-								<Status statusColor="gray">Fazendo</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há cerca de 2 meses</td>
-							<td>
-								<Status statusColor="yellow">Pausado</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há cerca de 2 meses</td>
-							<td>
-								<Status statusColor="red">Cancelado</Status>
-							</td>
-						</tr>
+						{cycles.map((cycle) => {
+							return (
+								<tr key={cycle.id}>
+									<td>{cycle.task}</td>
+									<td>{cycle.minutesAmount} minutos</td>
+									<td>Há cerca de {cycle.startDate.toISOString()}</td>
+									<td>
+										{cycle.finishedDate && (
+											<Status statusColor="green">Concluído</Status>
+										)}
+
+										{cycle.interruptedDate && (
+											<Status statusColor="red">Interrompido</Status>
+										)}
+
+										{!cycle.finishedDate && !cycle.interruptedDate && (
+											<Status statusColor="yellow">Em andamento</Status>
+										)}
+									</td>
+								</tr>
+							);
+						})}
 					</tbody>
 				</table>
 			</HistoryList>
